e945d17487ba4e1b963f7644abdf285bf1e203bf
Dnyaneshwar-dev <dnyaneshwar.ware@walchandsangli.ac.in>
2021-11-03 12:24:20 +0530

    Added Homepage

--- client/src/SenderSidebar.js
+++ client/src/SenderSidebar.js
@@ -17,9 +17,11 @@ import MicOffIcon from '@mui/icons-material/MicOff';
 import ScreenShareOutlinedIcon from '@mui/icons-material/ScreenShareOutlined';
 import VideocamOffOutlinedIcon from '@mui/icons-material/VideocamOffOutlined';
 import VideocamOutlinedIcon from '@mui/icons-material/VideocamOutlined';
-import {Widget, addResponseMessage, addLinkSnippet, addUserMessage, setQuickButtons} from 'react-chat-widget';
+import { Widget, addResponseMessage, addLinkSnippet, addUserMessage, setQuickButtons } from 'react-chat-widget';
 import adapter from 'webrtc-adapter';
-
+import { CopyToClipboard } from "react-copy-to-clipboard";
+import Tooltip from '@mui/material/Tooltip';
+import ContentCopyIcon from '@mui/icons-material/ContentCopy';
 import { useEffect } from 'react'
 import { store } from './redux/store'
 import { useState } from 'react'
@@ -31,22 +33,22 @@ const SenderSidebar = (props) => {
     const passcode = p.get('passcode')
     const handleNewUserMessage = newMessage => {
 
-        socket.emit('message', {message:newMessage, roomid:roomid});
+        socket.emit('message', { message: newMessage, roomid: roomid });
 
         // console.log(`New message incoming! ${newMessage}`);
         // Now send the message throught the backend API
         // addResponseMessage('hi')
     };
 
-    
+
 
     let screenpeer
-    async function shareScreen(){
+    async function shareScreen() {
         const screen = await navigator.mediaDevices.getDisplayMedia({ video: true, audio: true })
-        
+
         screenpeer = createPeer()
 
-        screen.getTracks().forEach(track => screenpeer.addTrack(track,screen))
+        screen.getTracks().forEach(track => screenpeer.addTrack(track, screen))
 
         function createPeer() {
             const peer = new RTCPeerConnection(config);
@@ -61,33 +63,33 @@ const SenderSidebar = (props) => {
             const payload = {
                 sdp: peer.localDescription
             };
-            socket.emit('publishscreen',{roomid: roomid, payload:payload })
-            
+            socket.emit('publishscreen', { roomid: roomid, payload: payload })
+
         }
     }
 
-    const handleMic = (stream) =>{
+    const handleMic = (stream) => {
         stream.getAudioTracks()[0].enabled = !(stream.getAudioTracks()[0].enabled);
     }
 
-    const handleVideo = (stream) =>{
+    const handleVideo = (stream) => {
         stream.getVideoTracks()[0].enabled = !(stream.getVideoTracks()[0].enabled);
     }
-    useEffect(()=>{
-        socket.on('screenoffer',async(payload)=>{
+    useEffect(() => {
+        socket.on('screenoffer', async (payload) => {
             console.log('screenoffer');
             const desc = new RTCSessionDescription(payload.sdp);
             await screenpeer.setRemoteDescription(desc).catch(e => console.log(e));
         })
 
-        socket.on('newmessage',({message})=>{
+        socket.on('newmessage', ({ message }) => {
             addResponseMessage(message)
         })
 
-    },[])
+    }, [])
 
-    const [video,toggleVideo] = useState(true)
-    const [mic,toggleMic] = useState(true)
+    const [video, toggleVideo] = useState(true)
+    const [mic, toggleMic] = useState(true)
 
     return (
         <>
@@ -109,52 +111,52 @@ const SenderSidebar = (props) => {
                 <br />
                 <Stack spacing={1}>
 
-                    <Box sx={{ display: 'flex', textAlign: "center", height: "70px", width: "70px" }}>
-
-                        <Button>
-                            <ChatIcon sx={{ color: "black", fontSize: 35 }} />
-                        </Button>
-                    </Box>
+                    
 
                     <Box sx={{ display: 'flex', textAlign: "center", height: "70px", width: "70px" }}>
-
-                        <Button>
-                            <HelpOutlineRoundedIcon sx={{ color: "black", fontSize: 35 }} />
-                        </Button>
-                    </Box>
-                    <Box sx={{ display: 'flex', textAlign: "center", height: "70px", width: "70px" }}>
-
-                        <Button onClick={()=>{
+                        <Tooltip title="Share Screen" aria-label="share" >
+                                
+                           
+                        <Button onClick={() => {
                             shareScreen();
                         }}>
                             <ScreenShareOutlinedIcon sx={{ color: "black", fontSize: 35 }} />
                         </Button>
+                        </Tooltip>
                     </Box>
                     <Box sx={{ display: 'flex', textAlign: "center", height: "70px", width: "70px" }}>
 
-                        <Button onClick={()=>{
+                        <Button onClick={() => {
                             const stream = store.getState().commonReducer.videostream
                             console.log(stream);
                             handleMic(stream)
                             toggleMic(!mic)
                         }}>
-                        {
-                            mic ? <MicNoneIcon sx={{ color: "red", fontSize: 35 }} />:<MicOffIcon sx={{ color: "black", fontSize: 35 }} />
-                        }  
+                            {
+                                mic ? <MicNoneIcon sx={{ color: "red", fontSize: 35 }} /> : <MicOffIcon sx={{ color: "black", fontSize: 35 }} />
+                            }
                         </Button>
                     </Box>
                     <Box sx={{ display: 'flex', textAlign: "center", height: "70px", width: "70px" }}>
-
-                        <Button onClick={()=>{
+                        
+                        <Button onClick={() => {
                             const stream = store.getState().commonReducer.videostream
-                            socket.emit('chat',{roomid:"abc",message:'mic'})
+                            socket.emit('chat', { roomid: "abc", message: 'mic' })
                             handleVideo(stream)
                             toggleVideo(!video)
                         }}>
-                        {
-                            video ? <VideocamOutlinedIcon sx={{ color: "red", fontSize: 35 }} /> : <VideocamOffOutlinedIcon sx={{ color: "black", fontSize: 35 }}/>
-                        }
+                            {
+                                video ? <VideocamOutlinedIcon sx={{ color: "red", fontSize: 35 }} /> : <VideocamOffOutlinedIcon sx={{ color: "black", fontSize: 35 }} />
+                            }
                         </Button>
+                    </Box>
+
+                    <Box sx={{ display: 'flex', textAlign: "center", height: "70px", width: "70px" }}>
+                        <CopyToClipboard text={`${window.location.protocol}//${window.location.hostname}/#/join?roomid=${roomid}&passcode=${passcode}`}>
+                            <Tooltip title="Copy Joining Link" aria-label="share" >
+                                <Button className="btn btn-outline-light" id="icons" > <ContentCopyIcon /></Button>
+                            </Tooltip>
+                        </CopyToClipboard>
                     </Box>
                     <Box sx={{ display: 'flex', textAlign: "center", height: "70px", width: "70px" }}>
                         <Widget
